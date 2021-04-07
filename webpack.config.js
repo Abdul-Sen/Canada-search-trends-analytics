f75983cc351ac4b396addb406ef232f575b78e32
Abdul-Fahm <Abdul@forahealthyme.com>
2021-04-07 19:33:29 -0400

    feat(scss): Configure webpack loader for scss

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,18 @@ module.exports = {
                 test: /\.(geojson|json)$/,
                 use: 'json-loader',
                 exclude: /node_modules/,
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              }        
         ]
     },
     plugins: [
