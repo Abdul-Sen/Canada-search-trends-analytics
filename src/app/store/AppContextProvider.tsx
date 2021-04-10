ebecb88d417dd7e28af26b45201df399c7c18ad5
Abdul-Fahm <Abdul@forahealthyme.com>
2021-04-10 11:47:01 -0400

    Revert "feat(toggletheme): toggle theme setup"
    This reverts commit f770673236a6dd7e495ededec7291767d1bddee9.

--- src/app/store/AppContextProvider.tsx
+++ src/app/store/AppContextProvider.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 
 import CoordinateProvider from '../context/CoordinateContext';
 import TodoProvider from '../context/TodoContext';
-import ThemeProvider from '../context/ThemeContext';
-
 import { combineComponents } from '../utils/combineComponents';
 
 // order matters, top components can be used by bottom components
 const providers: Array<React.FC<React.ReactNode>> = [
     CoordinateProvider,
     TodoProvider,
-    ThemeProvider
 ];
 
 export const AppContextProvider = combineComponents(...providers);
