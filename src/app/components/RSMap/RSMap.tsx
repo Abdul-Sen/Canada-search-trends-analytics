5a1af58c40122b3d31033e5b3f987a8e805d4fe6
Abdul-Fahm <Abdul@forahealthyme.com>
2021-04-15 06:42:43 -0400

    style(rsmap theme): stateful theme transitions

--- src/app/components/RSMap/RSMap.tsx
+++ src/app/components/RSMap/RSMap.tsx
@@ -12,10 +12,23 @@ import {
 
 import * as data from './canadaTested.geojson';
 
+
+const zoomProps: ZoomableGroupProps = {
+  center: [-100, 50],
+  zoom: 3,
+  maxZoom: 10,
+  translateExtent: [[-300, -300], [500, 400]]
+}
+const composableMapProps: ComposableMapProps = {
+  projection: "geoMercator",
+  id: "mapView"
+}
+
+const RSMap = () => {
+
 // RSM props
 const geoStyle = {
   default: {
-    fill: "#D6D6DA",
     outline: "none",
   },
   hover: {
@@ -27,31 +40,18 @@ const geoStyle = {
     outline: "none"
   }
 };
-const zoomProps: ZoomableGroupProps = {
-  center: [-100, 50],
-  zoom: 3,
-  maxZoom: 3,
-  translateExtent: [[-300, -300], [500, 400]]
-}
-const composableMapProps: ComposableMapProps = {
-  projection: "geoMercator",
-  height: 600,
-  width: 800
-}
-
-const RSMap = () => {
-
   return (
     <>
-      <div style={{ maxWidth: "100%" }}>
-        <ComposableMap {...composableMapProps}>
+      <div style={{ maxWidth: "100%", height:"100%" }}>
+        <ComposableMap {...composableMapProps} style={{ width: "100%", height: "100%"}}>
           {/* centering and zooming adjustments */}
           <ZoomableGroup {...zoomProps}>
-            <Geographies geography={data} stroke="#FFFFFF">
+            <Geographies geography={data} stroke="#949494" strokeWidth={0.2}> 
               {({ geographies }) =>
                 geographies.map((geo) => (
                   <Geography key={geo.rsmKey} geography={geo}
                     style={{ ...geoStyle }}
+                    className="geography"
                   />
                 ))
               }
