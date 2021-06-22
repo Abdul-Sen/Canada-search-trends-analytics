5bcf621960c62ab436ffdc320216e0204adc8099
Abdul-Fahm <Abdul@forahealthyme.com>
2021-06-21 22:27:52 -0400

    feat: DateCue component
    * DateCue based on figma design specs

--- src/app/components/DateCue/DateCue.tsx
+++ src/app/components/DateCue/DateCue.tsx
@@ -1,12 +1,27 @@
 import * as React from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { AccordianContext } from '../../context/AccordianContext'
 
 const DateCue = ()=> {
+    const {accordianState}  = useContext<AccordianContextType>(AccordianContext)
+    
+    useEffect(()=> {
+        console.log(accordianState.currentDate);
+    },[accordianState]);
+    const formattedDate = ()=>{ // we want 'Mon dd' 
+        const myDate = accordianState.currentDate.toUTCString().split(" ").splice(1,2).reverse().join(" ");
+        return myDate;
+    }
     
     return (
-        <p>
-            Toggle Theme
-        </p>
-    )
+        <div id="DateCueContainer">
+            <span id="DateCueValue">
+            {formattedDate()}
+            </span>
+        <br/>
+        </div>
+    );
 }
 
 export default DateCue;
